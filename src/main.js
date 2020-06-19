544b8aef071c55543eee0c175ca3076095441c8b
Oleksandr Demian <oleksandrovsky@gmail.com>
2020-06-19 09:54:46 +0200

    Theming

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import "./notifications";
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
+import config from "./utils/config";
+
+let themeName = config.get("theme");
+document.body.setAttribute("theme", themeName);
+
 const app = new App({
 	target: document.body,
 	props: {
--- src/sections/Configurations.svelte
+++ src/sections/Configurations.svelte
@@ -1,25 +1,44 @@
 <script>
 	import config from "../utils/config";
+
 	let commands = config.get("commands");
+	let themeName = config.get("theme");
 
-	$: {
+	const changeCommands = () => {
 		config.set("commands", commands);
-	}
+	};
 
+	const changeTheme = () => {
+		document.body.setAttribute("theme", themeName);
+		config.set("theme", themeName);
+	};
 </script>
 
 <div class="col-lg-5 col-md-12">
+	<!--	themes-->
+	<div class="m-3">
+		<div class="form-group">
+			<label for="searchIn"><b>Theme:</b></label>
+			<select class="form-control p-border" id="searchIn" bind:value={themeName} on:change={changeTheme}>
+				<option value="">Dark-white</option>
+				<option value="yellow-blue">Yellow-blue</option>
+				<option value="red-black">Red-black</option>
+			</select>
+		</div>
+	</div>
+
+	<!--	commands-->
 	<div class="m-3">
 		<div class="form-group">
 			<label for="searchIn"><b>Commands:</b></label>
 			<div class="form-check">
-				<input class="form-check-input" type="checkbox" bind:group={commands} value="git" id="git">
+				<input class="form-check-input" type="checkbox" bind:group={commands} value="git" id="git" on:change={changeCommands}>
 				<label class="form-check-label" for="git">
 					Git
 				</label>
 			</div>
 			<div class="form-check">
-				<input class="form-check-input" type="checkbox" bind:group={commands} value="degit" id="degit">
+				<input class="form-check-input" type="checkbox" bind:group={commands} value="degit" id="degit" on:change={changeCommands}>
 				<label class="form-check-label" for="degit">
 					Degit
 				</label>
